7fcc811d6b95f5f580bf58ab31bb141227130ff3
Benny Megidish <bennymegk@gmail.com>
2019-07-11 12:35:14 +0300

    fixed test import bug

--- test/src/response-parser.test.ts
+++ test/src/response-parser.test.ts
@@ -1,5 +1,5 @@
 import { ResponseParser } from '../../src/response-parser';
-import { Response } from '../../@types/src/response';
+import { Response } from '../../types/response';
 import { assert } from 'chai';
 
 let response: string = `{"status":"1","statusDesc":"","mitem":"משלוח דואר לחול - חבילה","service":"דואר אויר - ","cname":"ספרד","pcode":"3","weight":"20","qty":"2","prices":[{"Pweight":"עד 500 גרם","Pqty":"1","Pprice":"56.10","Ptotal":"112.20","commentsNo":"2","comments":[{"cno": "1"},{"cno": "2"}]}],"commTextsNo":"2","commTexts":[{"cno":"1","ctext":"המחיר"},{"cno":"2","ctext":"מיועד"}]}`;
